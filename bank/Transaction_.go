0b0797751913c9bc32cce761d40642e5dfe8a0aa
redmejia <preynaldorod@gmail.com>
2021-08-01 19:14:05 -0700

    make deposit to saving and checking

--- bank/Transaction_.go
+++ bank/Transaction_.go
@@ -9,15 +9,57 @@ import (
 	"github.com/smbank/logers"
 )
 
-func (t *Transaction) Deposit(w http.ResponseWriter) {
+// makeDeop make deposit to save or checking take a retrive bank account and make the update with the new balance
+func makeDepo(t *Transaction, retriveQuery, updateQuery string, w http.ResponseWriter) {
 	logr := logers.NewLogers()
+
+	tx, err := database.DB.Begin()
+	logr.CheckDBErr(err)
+	defer tx.Rollback()
+
+	row := tx.QueryRow(retriveQuery, t.Card, t.CvNumber)
+
+	var clientInfo Transaction
+	err = row.Scan(
+		&clientInfo.Balance,
+		&clientInfo.Card,
+		&clientInfo.CvNumber,
+	)
+	logr.CheckDBErr(err)
+
+	if clientInfo.Card == "" || clientInfo.CvNumber == 0 {
+		log.Println("Not found")
+	} else {
+		newBalance := clientInfo.Balance + t.Amount
+		_, err := tx.Exec(updateQuery, newBalance, clientInfo.Card, clientInfo.CvNumber)
+
+		logr.CheckDBErr(err)
+
+		var depoReport = struct {
+			Depo      bool    `json:"depo"`
+			DepoToAcc string  `json:"depo_to_acc"`
+			Amount    float64 `json:"amount"`
+		}{
+			Depo:      true,
+			DepoToAcc: t.AccountType,
+			Amount:    t.Amount,
+		}
+
+		w.Header().Add("Content-Type", "application/json")
+
+		data := json.NewEncoder(w)
+		err = data.Encode(depoReport)
+		logr.CheckErr(err)
+	}
+
+	err = tx.Commit()
+	logr.CheckDBErr(err)
+}
+
+func (t *Transaction) Deposit(w http.ResponseWriter) {
 	switch t.AccountType {
 	case "checking":
-		tx, err := database.DB.Begin()
-		logr.CheckDBErr(err)
-		defer tx.Rollback()
-
-		row := tx.QueryRow(`
+		retriveStm := `
 			SELECT 
 				balance,
 				card_number,
@@ -26,58 +68,38 @@ func (t *Transaction) Deposit(w http.ResponseWriter) {
 				checking_acc_type
 			WHERE
 				card_number = $1
-			AND card_cv = $2`,
-			t.Card,
-			t.CvNumber,
-		)
+			AND card_cv = $2
+			`
+		updateStm := `
+			UPDATE 
+				checking_acc_type
+			SET
+				balance = $1
+			WHERE 
+				card_number = $2 AND card_cv = $3
+			`
+		makeDepo(t, retriveStm, updateStm, w)
 
-		var clientInfo Transaction
-		err = row.Scan(&clientInfo.Balance, &clientInfo.Card, &clientInfo.CvNumber)
-		logr.CheckDBErr(err)
-
-		if clientInfo.Card == "" || clientInfo.CvNumber == 0 {
-			log.Println("Not found")
-		} else {
-			newBalance := clientInfo.Balance + t.Amount
-			_, err := tx.Exec(`
-				UPDATE 
-					checking_acc_type
-				SET
-					balance = $1
-				WHERE 
-					card_number = $2 AND card_cv = $3
-			`, newBalance, clientInfo.Card, clientInfo.CvNumber)
-
-			logr.CheckDBErr(err)
-
-			var depo = struct {
-				Depo   bool    `json:"depo"`
-				Amount float64 `json:"amount"`
-			}{
-				Depo:   true,
-				Amount: t.Amount,
-			}
-
-			w.Header().Add("Content-Type", "application/json")
-
-			data := json.NewEncoder(w)
-			err = data.Encode(depo)
-			logr.CheckErr(err)
-		}
-
-		err = tx.Commit()
-		logr.CheckDBErr(err)
 	case "saving":
-		return
-		// tx, err := database.DB.Begin()
-		// if err != nil {
-		// 	logr.Println(err)
-		// }
-		// defer tx.Rollback()
-
-		// err = tx.Commit()
-		// if err != nil {
-		// 	logr.Println(err)
-		// }
+		retriveStm := `
+			SELECT 
+				balance,
+				card_number,
+				card_cv
+			FROM
+				saving_acc_type
+			WHERE
+				card_number = $1
+			AND card_cv = $2
+			`
+		updateStm := `
+			UPDATE 
+				saving_acc_type
+			SET
+				balance = $1
+			WHERE 
+				card_number = $2 AND card_cv = $3
+			`
+		makeDepo(t, retriveStm, updateStm, w)
 	}
 }
